040994cd5162ac43eccf35a39e69bdaa79511538
Blackscs <marioosantos238412@gmail.com>
2023-08-11 23:59:39 -0300

    upgrade login, tela de compra, e index

---
+++ telaDeCompras.js
@@ -0,0 +1,36 @@
+function escolherCorPreto() {
+    document.getElementById('img-principal').src = 'IMAGENS/IphonePreto.png';
+    document.getElementById('textoTitulo').textContent = "Apple iPhone 14 Pro Max 128GB Preto-Espacial 6,7” 48MP";
+}
+function escolherCorRoxo() {
+    document.getElementById('img-principal').src = 'IMAGENS/IphoneRoxo.png';
+    document.getElementById('textoTitulo').textContent = "Apple iPhone 14 Pro Max 128GB Roxo-Profundo 6,7” 48MP";
+}
+function escolherCorBranco() {
+    document.getElementById('img-principal').src = 'IMAGENS/IphoneBranco.png';
+    document.getElementById('textoTitulo').textContent = "Apple iPhone 14 Pro Max 128GB Prata 6,7” 48MP";
+}
+function escolherCorDourado() {
+    document.getElementById('img-principal').src = 'IMAGENS/IphoneDourado.png';
+    document.getElementById('textoTitulo').textContent = "Apple iPhone 14 Pro Max 128GB Dourado 6,7” 48MP";
+}
+
+
+function TB1() {
+    document.getElementById("arm-int").textContent = "Armazenamento Interno: 1TB";
+}
+function GB128() {
+    document.getElementById("arm-int").textContent = "Armazenamento Interno: 128GB";
+}
+function GB256() {
+    document.getElementById("arm-int").textContent = "Armazenamento Interno: 256GB";
+}
+function GB512() {
+    document.getElementById("arm-int").textContent = "Armazenamento Interno: 512GB";
+}
+
+function out(){
+    document.getElementById("arm-int").textContent = "Armazenamento Interno:";
+}
+
+    
