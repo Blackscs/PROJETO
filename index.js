92609f47ed1961e7a09d2e44cd7deb7f6b30a1d0
Blackscs <marioosantos238412@gmail.com>
2023-08-14 01:23:23 -0300

    ajustes

--- index.js
+++ index.js
@@ -1,45 +1,38 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("button-menu").addEventListener("click", function() {
-        var barraLateral = document.querySelector(".container-barraLateral");
-        barraLateral.style.left = "0"; // Mostra a barra lateral definindo o valor "left" para 0
-    });
+document.getElementById("button-menu").addEventListener("click", function() {
+    var barraLateral = document.querySelector(".container-barraLateral");
+    barraLateral.style.left = "0"; // Mostra a barra lateral definindo o valor "left" para 0
+});
 
-    document.getElementById("closeMenuNav").addEventListener("click", function() {
-        var barraLateral = document.querySelector(".container-barraLateral");
-        barraLateral.style.left = "-320px"; // Mostra a barra lateral definindo o valor "left" para 0
-    });
+document.getElementById("closeMenuNav").addEventListener("click", function() {
+    var barraLateral = document.querySelector(".container-barraLateral");
+    barraLateral.style.left = "-320px"; // Mostra a barra lateral definindo o valor "left" para 0
+});
 
-    const controls = document.querySelectorAll(".control"); //selecionando os botoes
-    let currentItem = 0;
-    const items = document.querySelectorAll(".item");
-    const maxItems = items.length;
 
-    controls.forEach((control) => {
-        control.addEventListener("click", (e) => {
-            isLeft = e.target.classList.contains("arrow-left");
+const controls = document.querySelectorAll(".control"); //selecionando os botoes
+let currentItem = 0;
+const items = document.querySelectorAll(".item");
+const maxItems = items.length;
 
-                if (isLeft) {
-                    currentItem -= 2;
-                } else {
-                    currentItem += 2;
-                }
+controls.forEach((control) => {
+    control.addEventListener("click", (e) => {
+        isLeft = e.target.classList.contains("arrow-left");
 
-                if (currentItem >= maxItems) {
-                    currentItem = 0;
-                }
+            if (isLeft) {
+                currentItem -= 4;
+            } else {
+                currentItem += 4;
+            }
 
-                if (currentItem < 0) {
-                    currentItem = maxItems - 1;
-                }
+            if (currentItem >= maxItems) {
+                currentItem = 0;
+            }
 
-                items.forEach((item) => item.classList.remove("current-item"));
+            if (currentItem < 0) {
+                currentItem = maxItems - 1;
+            }
 
-                items[currentItem].scrollIntoView({
-                    behavior: "smooth",
-                    inline: "center"
-                });
-
-            items[currentItem].classList.add("current-item");
-        });
+            items.forEach((item) => item.classList.remove("current-item"));
+        items[currentItem].classList.add("current-item");
     });
 });
